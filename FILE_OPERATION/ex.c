54957071327099ed8ceb0bc13c6f4a0a8b4e9b29
Devidutta99 <127765993+Devidutta99@users.noreply.github.com>
2023-07-06 11:35:51 +0530

    Add files via upload

---
+++ FILE_OPERATION/ex.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<fcntl.h>
+
+int main(){
+	int s;
+	s=open("hello.txt",O_RDONLY);
+	if(s<0){
+		printf("Unable to create\n");
+		perror("error message:");
+	}
+	else{
+		printf("Successfully created. S value:%d\n",s);
+	}
+	getchar();
+}
+
